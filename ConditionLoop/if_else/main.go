ca52fd46574249d2268a4e41b893febd17b2c02b
gogoing-code <1378412050@qq.com>
2021-10-01 18:20:48 +0800

    first commit

--- ConditionLoop/if_else/main.go
+++ ConditionLoop/if_else/main.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-func main(){
-	a:=100
-	if a < 20{
+func main() {
+	a := 100
+	if a < 20 {
 		fmt.Println("a小于20")
-	}else{
+	} else {
 		fmt.Println("a大于20")
 	}
 	fmt.Printf("a 的值为 : %d", a)
--- ConditionLoop/switch/main.go
+++ ConditionLoop/switch/main.go
@@ -8,23 +8,27 @@ func main() {
 	var marks int = 90
 
 	switch marks {
-	case 90: grade = "A"
-	case 80: grade = "B"
-	case 50,60,70 : grade = "C"
-	default: grade = "D"
+	case 90:
+		grade = "A"
+	case 80:
+		grade = "B"
+	case 50, 60, 70:
+		grade = "C"
+	default:
+		grade = "D"
 	}
 
 	switch {
-	case grade == "A" :
-		fmt.Printf("优秀!\n" )
-	case grade == "B", grade == "C" :
-		fmt.Printf("良好\n" )
-	case grade == "D" :
-		fmt.Printf("及格\n" )
+	case grade == "A":
+		fmt.Printf("优秀!\n")
+	case grade == "B", grade == "C":
+		fmt.Printf("良好\n")
+	case grade == "D":
+		fmt.Printf("及格\n")
 	case grade == "F":
-		fmt.Printf("不及格\n" )
+		fmt.Printf("不及格\n")
 	default:
-		fmt.Printf("差\n" );
+		fmt.Printf("差\n")
 	}
-	fmt.Printf("你的等级是 %s\n", grade );
+	fmt.Printf("你的等级是 %s\n", grade)
 }
